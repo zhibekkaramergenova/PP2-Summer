261516ef9815f061a2bb27d056eb72d80ae3f2e8
zhibekkaramergenova <80530342+zhibekkaramergenova@users.noreply.github.com>
2021-07-08 14:30:57 +0600

    Update 1.py

--- TSIS4/task2/1.py
+++ TSIS4/task2/1.py
@@ -1,1 +1,12 @@
-
+import re
+pattern=re.compile(r'<[a-z][\w\-\_\.]+@[a-zA-Z]+\.[a-z]{1,3}>')
+resname=[]
+resemail=[]
+n=int(input())
+for i in range(n):
+    name,email=input().split()
+    if pattern.search(email):
+       resname.append(name)
+       resemail.append(email)
+for i in range(len(resname)):
+    print(resname[i],resemail[i])
